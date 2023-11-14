27ab76ea31a1b96bd12aa902e11c9a2d55609e19
francismiko <1485059943@qq.com>
2023-11-14 11:39:06 +0800

    chore: update

--- assignments/homework-06/src/App.vue
+++ assignments/homework-06/src/App.vue
@@ -1,27 +1,30 @@
 <template>
   <div class="container">
+    <!-- 可选职位 -->
     <div class="box">
       <div class="header">可选职位</div>
       <div class="positions">
         <div v-for="position in positions" :key="position"
-          :class="{ 'position': true, 'highlighted': temporarySelection.includes(position) }"
-          @click="addToTemporarySelection(position, false)">
+          :class="{ 'position': true, 'highlighted': temporarySelection.has(position) }"
+          @click="toggleTemporarySelection(position)">
           {{ position }}
         </div>
       </div>
     </div>
 
+    <!-- 操作按钮 -->
     <div class="actions">
       <button @click="moveSelected(true)">》</button>
       <button @click="moveSelected(false)">《</button>
     </div>
 
+    <!-- 已选职位 -->
     <div class="box">
       <div class="header">已选职位</div>
       <div class="selected-positions">
-        <div v-for="position in selectedPositions" :key="position"
-          :class="{ 'position': true, 'highlighted': temporarySelection.includes(position) }"
-          @click="addToTemporarySelection(position, true)">
+        <div v-for="position in Array.from(selectedPositions)" :key="position"
+          :class="{ 'position': true, 'highlighted': temporarySelection.has(position) }"
+          @click="toggleTemporarySelection(position)">
           {{ position }}
         </div>
       </div>
@@ -29,42 +32,50 @@
   </div>
 </template>
 
+
 <script setup>
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 const positions = ref(['前端开发', '后端开发', '全栈开发', '测试开发', '运维开发', '网络管理']);
-const selectedPositions = ref([]);
-const temporarySelection = ref([]);
+const selectedPositions = reactive(new Set());
+const temporarySelection = reactive(new Set());
 
-function addToTemporarySelection(position, isSelected) {
-  if (!temporarySelection.value.includes(position)) {
-    temporarySelection.value.push(position);
+function toggleTemporarySelection(position) {
+  if (temporarySelection.has(position)) {
+    temporarySelection.delete(position);
   } else {
-    temporarySelection.value = temporarySelection.value.filter(p => p !== position);
+    temporarySelection.add(position);
   }
 }
 
 function moveSelected(toSelect) {
   if (toSelect) {
-    temporarySelection.value.forEach(position => {
-      if (!selectedPositions.value.includes(position)) {
-        selectedPositions.value.push(position);
-        positions.value = positions.value.filter(p => p !== position);
-      }
-    });
+    selectPositions();
   } else {
-    temporarySelection.value.forEach(position => {
-      if (selectedPositions.value.includes(position)) {
-        positions.value.push(position);
-      }
-    });
-    selectedPositions.value = selectedPositions.value.filter(
-      position => !temporarySelection.value.includes(position)
-    );
+    unselectPositions();
   }
-  temporarySelection.value = [];
+  temporarySelection.clear();
+}
+
+function selectPositions() {
+  temporarySelection.forEach(position => {
+    if (!selectedPositions.has(position)) {
+      selectedPositions.add(position);
+      positions.value = positions.value.filter(p => p !== position);
+    }
+  });
+}
+
+function unselectPositions() {
+  temporarySelection.forEach(position => {
+    if (selectedPositions.has(position)) {
+      selectedPositions.delete(position);
+      positions.value.push(position);
+    }
+  });
 }
 </script>
+
 
 <style scoped>
 .container {
